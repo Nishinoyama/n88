9f36e3d349896a20539d04db28e51a4974e978ad
Nishinoyama <begoingto1414@gmail.com>
2023-01-01 16:10:19 +0900

    fix: register load is writing value to it, read is load of its value

--- src/register.rs
+++ src/register.rs
@@ -2,8 +2,8 @@ pub trait RegisterCode {}
 
 pub trait RegisterSet<R: RegisterCode> {
     type Size;
-    fn load_of(&self, code: R) -> Self::Size;
-    fn write_of(&mut self, code: R, bits: Self::Size);
+    fn read_of(&self, code: R) -> Self::Size;
+    fn load_of(&mut self, code: R, bits: Self::Size);
 }
 
 pub trait RegisterFlag {
@@ -25,8 +25,8 @@ pub trait RegisterFlag {
 
 pub trait Register {
     type Size;
-    fn write(&mut self, bits: Self::Size);
-    fn load(&self) -> Self::Size;
+    fn load(&mut self, bits: Self::Size);
+    fn read(&self) -> Self::Size;
 }
 
 #[derive(Debug, Default, Clone)]
@@ -37,39 +37,39 @@ pub struct Register16 {
 impl Register for Register16 {
     type Size = u16;
 
-    fn write(&mut self, bits: Self::Size) {
+    fn load(&mut self, bits: Self::Size) {
         self.bits = bits;
     }
 
-    fn load(&self) -> Self::Size {
+    fn read(&self) -> Self::Size {
         self.bits
     }
 }
 
 pub(crate) struct Register16In8Writer<'a> {
-    register: &'a mut Register16,
-    low: bool,
+    pub register: &'a mut Register16,
+    pub low: bool,
 }
 
 impl<'a> Register16In8Writer<'a> {
-    fn write(&mut self, bits: u8) {
+    pub(crate) fn write(&mut self, bits: u8) {
         let t = if self.low {
-            self.register.load() & (!0x00ff) | (bits as u16)
+            self.register.read() & (!0x00ff) | (bits as u16)
         } else {
-            self.register.load() & (!0xff00) | ((bits as u16) << 8)
+            self.register.read() & (!0xff00) | ((bits as u16) << 8)
         };
-        self.register.write(t)
+        self.register.load(t)
     }
 }
 
 pub(crate) struct Register16In8Loader<'a> {
-    register: &'a Register16,
-    low: bool,
+    pub register: &'a Register16,
+    pub low: bool,
 }
 
 impl<'a> Register16In8Loader<'a> {
-    fn load(&self) -> u8 {
-        let t = self.register.load();
+    pub(crate) fn load(&self) -> u8 {
+        let t = self.register.read();
         if self.low {
             (t & 0x00ff) as u8
         } else {
@@ -115,17 +115,17 @@ mod tests {
     impl RegisterSet<Register16Code> for Register16Set {
         type Size = u16;
 
-        fn load_of(&self, code: Register16Code) -> Self::Size {
+        fn read_of(&self, code: Register16Code) -> Self::Size {
             match code {
-                Register16Code::AF => self.af.load(),
-                Register16Code::HL => self.hl.load(),
+                Register16Code::AF => self.af.read(),
+                Register16Code::HL => self.hl.read(),
             }
         }
 
-        fn write_of(&mut self, code: Register16Code, bits: Self::Size) {
+        fn load_of(&mut self, code: Register16Code, bits: Self::Size) {
             match code {
-                Register16Code::AF => self.af.write(bits),
-                Register16Code::HL => self.hl.write(bits),
+                Register16Code::AF => self.af.load(bits),
+                Register16Code::HL => self.hl.load(bits),
             }
         }
     }
@@ -133,7 +133,7 @@ mod tests {
     impl RegisterSet<Register8Code> for Register16Set {
         type Size = u8;
 
-        fn load_of(&self, code: Register8Code) -> Self::Size {
+        fn read_of(&self, code: Register8Code) -> Self::Size {
             let register = match code {
                 Register8Code::A => &self.af,
                 Register8Code::H | Register8Code::L => &self.hl,
@@ -144,7 +144,7 @@ mod tests {
             }.load()
         }
 
-        fn write_of(&mut self, code: Register8Code, bits: Self::Size) {
+        fn load_of(&mut self, code: Register8Code, bits: Self::Size) {
             let register = match code {
                 Register8Code::A => &mut self.af,
                 Register8Code::H | Register8Code::L => &mut self.hl,
@@ -204,20 +204,20 @@ mod tests {
     #[test]
     fn register_modifier() {
         let mut reg = Register16::default();
-        reg.write(0x1234);
-        assert_eq!(reg.load(), 0x1234);
+        reg.load(0x1234);
+        assert_eq!(reg.read(), 0x1234);
         let mut reg_mod = Register16In8Writer {
             register: &mut reg,
             low: true,
         };
         reg_mod.write(0x56);
-        assert_eq!(reg.load(), 0x1256);
+        assert_eq!(reg.read(), 0x1256);
         let mut reg_mod = Register16In8Writer {
             register: &mut reg,
             low: false,
         };
         reg_mod.write(0x78);
-        assert_eq!(reg.load(), 0x7856);
+        assert_eq!(reg.read(), 0x7856);
     }
 
     #[test]
@@ -225,13 +225,13 @@ mod tests {
         use self::Register8Code::*;
         use self::Register16Code::*;
         let mut regs = Register16Set::default();
-        regs.write_of(AF, 0x1234);
-        assert_eq!(regs.load_of(AF), 0x1234);
-        regs.write_of(A, 0x56);
-        assert_eq!(regs.load_of(AF), 0x5634);
-        regs.write_of(HL, 0x9abc);
-        assert_eq!(regs.load_of(H), 0x9a);
-        assert_eq!(regs.load_of(L), 0xbc);
+        regs.load_of(AF, 0x1234);
+        assert_eq!(regs.read_of(AF), 0x1234);
+        regs.load_of(A, 0x56);
+        assert_eq!(regs.read_of(AF), 0x5634);
+        regs.load_of(HL, 0x9abc);
+        assert_eq!(regs.read_of(H), 0x9a);
+        assert_eq!(regs.read_of(L), 0xbc);
     }
 
     #[test]
@@ -239,7 +239,7 @@ mod tests {
         use self::Register16Code::*;
         use self::Flag::*;
         let mut regs = Register16Set::default();
-        regs.write_of(AF, 0x0000);
+        regs.load_of(AF, 0x0000);
         regs.flag_set(Overflow);
         assert!(regs.flag_get(Overflow));
         regs.flag_set(Neg);
