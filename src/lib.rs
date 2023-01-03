ede3014191762040888637cf0873eb7385e2251a
Nishinoyama <begoingto1414@gmail.com>
2023-01-04 01:15:03 +0900

    new: bitwise operation packed trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+use std::ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign, Not};
+
+pub trait BitwiseOps:
+    BitAnd<Output = Self>
+    + BitOr<Output = Self>
+    + BitAndAssign
+    + BitOrAssign
+    + Not<Output = Self>
+    + Eq
+    + PartialEq
+    + Copy
+{
+}
+impl BitwiseOps for u8 {}
+impl BitwiseOps for u16 {}
+impl BitwiseOps for u32 {}
+impl BitwiseOps for u64 {}
+impl BitwiseOps for usize {}
+
 pub mod register;
 
 pub mod instruction;
