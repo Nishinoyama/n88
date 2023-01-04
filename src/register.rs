e31962a86ef40d3978ab7c7b73583abccfd11693
Nishinoyama <begoingto1414@gmail.com>
2023-01-04 23:26:09 +0900

    new: bitwise-masked Register, fix: loader is reader

--- src/register.rs
+++ src/register.rs
@@ -1,13 +1,13 @@
-pub trait RegisterCode {
-    type Size;
-}
+use crate::BitwiseOps;
+
+pub trait RegisterCode {}
 
 #[allow(clippy::needless_lifetimes)]
-pub trait RegisterSet<R: RegisterCode> {
-    type Loader<'a>
+pub trait RegisterSet<R> {
+    type Loader<'a>: RegisterLoader
     where
         Self: 'a;
-    type Reader<'a>
+    type Reader<'a>: RegisterReader
     where
         Self: 'a;
     fn loader_of<'a>(&'a mut self, code: R) -> Self::Loader<'a>;
@@ -20,66 +20,8 @@ pub trait Register {
     fn read(&self) -> Self::Size;
 }
 
-#[derive(Debug, Default, Clone)]
-pub struct Register16 {
-    bits: u16,
-}
-
-impl Register for Register16 {
-    type Size = u16;
-
-    fn load(&mut self, bits: Self::Size) {
-        self.bits = bits;
-    }
-
-    fn read(&self) -> Self::Size {
-        self.bits
-    }
-}
-
-pub trait RegisterLoader {
-    type Size;
+pub trait RegisterLoader: RegisterReader {
     fn load(&mut self, bits: Self::Size);
-}
-
-pub struct Register16In8Loader<'a> {
-    pub register: &'a mut Register16,
-    pub low: bool,
-}
-
-impl<'a> Register16In8Loader<'a> {
-    pub fn new(register: &'a mut Register16, low: bool) -> Self {
-        Self { register, low }
-    }
-}
-
-impl<'a> RegisterLoader for Register16In8Loader<'a> {
-    type Size = u8;
-    fn load(&mut self, bits: Self::Size) {
-        let t = if self.low {
-            self.register.read() & (!0x00ff) | (bits as u16)
-        } else {
-            self.register.read() & (!0xff00) | ((bits as u16) << 8)
-        };
-        self.register.load(t)
-    }
-}
-
-pub struct Register16Loader<'a> {
-    pub register: &'a mut Register16,
-}
-
-impl<'a> Register16Loader<'a> {
-    pub fn new(register: &'a mut Register16) -> Self {
-        Self { register }
-    }
-}
-
-impl<'a> RegisterLoader for Register16Loader<'a> {
-    type Size = u16;
-    fn load(&mut self, bits: u16) {
-        self.register.load(bits)
-    }
 }
 
 pub trait RegisterReader {
@@ -87,48 +29,153 @@ pub trait RegisterReader {
     fn read(&self) -> Self::Size;
 }
 
-pub struct Register16In8Reader<'a> {
-    pub register: &'a Register16,
-    pub low: bool,
+#[derive(Debug)]
+pub struct MaskedRegisterLoader<B, L> {
+    loader: L,
+    mask: B,
 }
 
-impl<'a> RegisterReader for Register16In8Reader<'a> {
-    type Size = u8;
+impl<B: BitwiseOps, L: RegisterLoader<Size = B>> MaskedRegisterLoader<B, L> {
+    pub fn new(loader: L, mask: B) -> Self {
+        Self { loader, mask }
+    }
+}
+
+impl<B: BitwiseOps, L: RegisterLoader<Size = B>> RegisterReader for MaskedRegisterLoader<B, L> {
+    type Size = B;
+
     fn read(&self) -> Self::Size {
-        let t = self.register.read();
-        if self.low {
-            (t & 0x00ff) as u8
-        } else {
-            (t >> 8) as u8
+        self.loader.read() & self.mask
+    }
+}
+
+pub mod typical {
+    use super::*;
+
+    #[derive(Debug, Default, Clone)]
+    pub struct Register16 {
+        bits: u16,
+    }
+
+    impl Register for Register16 {
+        type Size = u16;
+
+        fn load(&mut self, bits: Self::Size) {
+            self.bits = bits;
+        }
+
+        fn read(&self) -> Self::Size {
+            self.bits
         }
     }
-}
 
-impl<'a> Register16In8Reader<'a> {
-    pub fn new(register: &'a Register16, low: bool) -> Self {
-        Self { register, low }
+    impl<B: BitwiseOps, L: RegisterLoader<Size = B>> RegisterLoader for MaskedRegisterLoader<B, L> {
+        fn load(&mut self, bits: Self::Size) {
+            let res = bits & self.mask | self.loader.read() & !self.mask;
+            self.loader.load(res)
+        }
     }
-}
 
-pub struct Register16Reader<'a> {
-    pub register: &'a Register16,
-}
-
-impl<'a> RegisterReader for Register16Reader<'a> {
-    type Size = u16;
-    fn read(&self) -> Self::Size {
-        self.register.read()
+    pub struct Register16In8Loader<'a> {
+        register: &'a mut Register16,
+        low: bool,
     }
-}
 
-impl<'a> Register16Reader<'a> {
-    pub fn new(register: &'a Register16) -> Self {
-        Self { register }
+    impl<'a> Register16In8Loader<'a> {
+        pub fn new(register: &'a mut Register16, low: bool) -> Self {
+            Self { register, low }
+        }
+    }
+
+    impl<'a> RegisterReader for Register16In8Loader<'a> {
+        type Size = u8;
+
+        fn read(&self) -> Self::Size {
+            Register16In8Reader::new(self.register, self.low).read()
+        }
+    }
+
+    impl<'a> RegisterLoader for Register16In8Loader<'a> {
+        fn load(&mut self, bits: Self::Size) {
+            let t = if self.low {
+                self.register.read() & (!0x00ff) | (bits as u16)
+            } else {
+                self.register.read() & (!0xff00) | ((bits as u16) << 8)
+            };
+            self.register.load(t)
+        }
+    }
+
+    #[derive(Debug)]
+    pub struct Register16Loader<'a> {
+        pub register: &'a mut Register16,
+    }
+
+    impl<'a> Register16Loader<'a> {
+        pub fn new(register: &'a mut Register16) -> Self {
+            Self { register }
+        }
+    }
+
+    impl<'a> RegisterReader for Register16Loader<'a> {
+        type Size = u16;
+
+        fn read(&self) -> Self::Size {
+            self.register.read()
+        }
+    }
+
+    impl<'a> RegisterLoader for Register16Loader<'a> {
+        fn load(&mut self, bits: u16) {
+            self.register.load(bits)
+        }
+    }
+
+    pub struct Register16In8Reader<'a> {
+        register: &'a Register16,
+        low: bool,
+    }
+
+    impl<'a> RegisterReader for Register16In8Reader<'a> {
+        type Size = u8;
+        fn read(&self) -> Self::Size {
+            let t = self.register.read();
+            if self.low {
+                (t & 0x00ff) as u8
+            } else {
+                (t >> 8) as u8
+            }
+        }
+    }
+
+    impl<'a> Register16In8Reader<'a> {
+        pub fn new(register: &'a Register16, low: bool) -> Self {
+            Self { register, low }
+        }
+    }
+
+    #[derive(Debug)]
+    pub struct Register16Reader<'a> {
+        register: &'a Register16,
+    }
+
+    impl<'a> RegisterReader for Register16Reader<'a> {
+        type Size = u16;
+        fn read(&self) -> Self::Size {
+            self.register.read()
+        }
+    }
+
+    impl<'a> Register16Reader<'a> {
+        pub fn new(register: &'a Register16) -> Self {
+            Self { register }
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::typical::*;
     use super::*;
 
     #[derive(Default, Debug)]
@@ -155,14 +202,6 @@ mod tests {
                 Register8Code::L => true,
             }
         }
-    }
-
-    impl RegisterCode for Register16Code {
-        type Size = u16;
-    }
-
-    impl RegisterCode for Register8Code {
-        type Size = u8;
     }
 
     #[allow(clippy::needless_lifetimes)]
@@ -211,49 +250,15 @@ mod tests {
         Neg = 2,
     }
 
-    pub(crate) struct Register16FlagWriter<'a> {
-        register: &'a mut Register16,
-        flag_bit: u8,
-    }
-
-    impl<'a> Register16FlagWriter<'a> {
-        fn write(&mut self, set: bool) {
-            let t = Register16In8Reader {
-                register: self.register,
-                low: true,
-            }
-            .read();
-            if set {
-                Register16In8Loader {
-                    register: self.register,
-                    low: true,
-                }
-                .load(t | self.flag_bit);
-            } else {
-                Register16In8Loader {
-                    register: self.register,
-                    low: true,
-                }
-                .load(t & !self.flag_bit);
-            }
-        }
-    }
-
     #[test]
     fn register_modifier() {
         let mut reg = Register16::default();
         reg.load(0x1234);
         assert_eq!(reg.read(), 0x1234);
-        let mut reg_mod = Register16In8Loader {
-            register: &mut reg,
-            low: true,
-        };
+        let mut reg_mod = Register16In8Loader::new(&mut reg, true);
         reg_mod.load(0x56);
         assert_eq!(reg.read(), 0x1256);
-        let mut reg_mod = Register16In8Loader {
-            register: &mut reg,
-            low: false,
-        };
+        let mut reg_mod = Register16In8Loader::new(&mut reg, false);
         reg_mod.load(0x78);
         assert_eq!(reg.read(), 0x7856);
     }
@@ -270,5 +275,13 @@ mod tests {
         regs.loader_of(HL).load(0x9abc);
         assert_eq!(regs.reader_of(H).read(), 0x9a);
         assert_eq!(regs.reader_of(L).read(), 0xbc);
+    }
+
+    #[test]
+    fn bitwise_loader() {
+        let mut reg = Register16::default();
+        let mut loader = MaskedRegisterLoader::new(Register16Loader::new(&mut reg), 0xf0f0);
+        loader.load(0x1234);
+        assert_eq!(loader.read(), 0x1030);
     }
 }
