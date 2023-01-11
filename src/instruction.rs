6f03b55fb07de87f0c9ee5e5649ae999aa093132
Nishinoyama <begoingto1414@gmail.com>
2023-01-11 14:47:18 +0900

    add: typical instructions

--- src/instruction.rs
+++ src/instruction.rs
@@ -4,6 +4,8 @@ pub trait Instruction<CPU> {
 
 pub mod typical {
     use super::*;
+    use crate::addressing::Addressing;
+    use crate::alu::ALU;
     use crate::cpu::*;
     use crate::register::*;
 
@@ -12,9 +14,9 @@ pub mod typical {
     }
 
     impl<CPU, A> Instruction<CPU> for Jump<A>
-        where
-            CPU: CPUProgramCounter<MemoryAddress = A>,
-            A: Copy,
+    where
+        CPU: CPUProgramCounter<MemoryAddress = A>,
+        A: Copy,
     {
         fn execute(self, cpu: &mut CPU) {
             cpu.program_counter_loader().load(self.address);
@@ -32,9 +34,9 @@ pub mod typical {
     }
 
     impl<CPU, B> Instruction<CPU> for Push<B>
-        where
-            CPU: CPUStackPointer<MemoryData = B>,
-            B: Copy,
+    where
+        CPU: CPUStackPointer<MemoryData = B>,
+        B: Copy,
     {
         fn execute(self, cpu: &mut CPU) {
             cpu.push(self.data)
@@ -48,13 +50,12 @@ pub mod typical {
     }
 
     pub struct Pop<C> {
-        dst: C
+        dst: C,
     }
 
     impl<CPU, C, B> Instruction<CPU> for Pop<C>
-        where
-            CPU: CPUStackPointer<MemoryData = B> + RegisterSet<C, Size = B>,
-    {
+    where
+        CPU: CPUStackPointer<MemoryData = B> + RegisterSet<C, Size = B>,
     {
         fn execute(self, cpu: &mut CPU) {
             let src = cpu.pop();
@@ -68,14 +69,98 @@ pub mod typical {
     }
 
     impl<CPU, F, I> Instruction<CPU> for Condition<F, I>
-        where
-            F: Fn(&CPU) -> bool,
-            I: Instruction<CPU>,
+    where
+        F: FnOnce(&CPU) -> bool,
+        I: Instruction<CPU>,
     {
         fn execute(self, cpu: &mut CPU) {
             if (self.cond)(cpu) {
                 self.then.execute(cpu);
             }
+        }
+    }
+
+    pub struct Load<C, A> {
+        dst: C,
+        src: A,
+    }
+
+    impl<C, A> Load<C, A> {
+        pub fn new(dst: C, src: A) -> Self {
+            Self { dst, src }
+        }
+    }
+
+    impl<CPU, C, B, A> Instruction<CPU> for Load<C, A>
+    where
+        CPU: RegisterSet<C, Size = B>,
+        C: Copy,
+        A: Addressing<CPU, Size = B> + Copy,
+    {
+        fn execute(self, cpu: &mut CPU) {
+            let bits = self.src.value(cpu);
+            cpu.loader_of(self.dst).load(bits);
+        }
+    }
+
+    pub struct Store<D, S> {
+        dst: D,
+        src: S,
+    }
+
+    impl<D, S> Store<D, S> {
+        pub fn new(dst: D, src: S) -> Self {
+            Self { dst, src }
+        }
+    }
+
+    impl<CPU, D, S, A, B> Instruction<CPU> for Store<D, S>
+    where
+        CPU: CPUMemory<MemoryAddress = A, MemoryData = B>,
+        D: Addressing<CPU, Size = A> + Copy,
+        S: Addressing<CPU, Size = B> + Copy,
+    {
+        fn execute(self, cpu: &mut CPU) {
+            let dst = self.dst.value(cpu);
+            let src = self.src.value(cpu);
+            cpu.memory_store(dst, src);
+        }
+    }
+
+    pub struct Arithmetic<C, F, D, L> {
+        control: C,
+        flags: Vec<F>,
+        dst: D,
+        rhs: L,
+    }
+
+    impl<C, F, D, L> Arithmetic<C, F, D, L> {
+        pub fn new(control: C, flags: Vec<F>, dst: D, rhs: L) -> Self {
+            Self {
+                control,
+                flags,
+                dst,
+                rhs,
+            }
+        }
+    }
+
+    impl<CPU, A, C, F, D, L, B, G> Instruction<CPU> for Arithmetic<C, F, D, L>
+    where
+        CPU: CPUAccumulator<AccSize = B>
+            + CPUFlagRegister<ALU = A, FlagRegisterSize = G>
+            + RegisterSet<D, Size = B>,
+        A: ALU<Data = B, Control = C, Flag = F>,
+        C: Copy,
+        F: Copy,
+        L: Addressing<CPU, Size = B>,
+        G: From<A::FlagSet>,
+    {
+        fn execute(self, cpu: &mut CPU) {
+            let rhs = self.rhs.value(cpu);
+            let (res, flag) = cpu.alu_acc_op(self.control, rhs);
+            cpu.flag_loader_mask_slice(&self.flags).load(flag.into());
+            cpu.loader_of(self.dst).load(res);
         }
     }
 }
@@ -94,7 +179,7 @@ mod tests {
         LoadA(u8),
         LoadB(u8),
         Add,
-        Etc(Box<dyn Fn(&mut CPU)>),
+        Etc(Box<dyn FnOnce(&mut CPU)>),
     }
 
     impl Instruction<CPU8> for Instructions<CPU8> {
